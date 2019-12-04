07273d2df9572c94ea82b353a33ea73cff6a7de4
Hao Wu <hwu@atheerair.com>
2019-12-03 18:46:06 -0800

    added ability to set display name in order to identify users

--- atheer.js
+++ atheer.js
@@ -2,10 +2,14 @@ import {
     setAudioMuted,
     setVideoMuted
 } from './react/features/base/media';
+import { updateSettings } from './react/features/base/settings/actions';
+
+export const displayNameSplit = ':';
 
 export const ATHEER_MESSAGE = {
     TEST_MESSAGE: 'testMessage',
     CONFERENCE_INTIALIZED: 'conferenceInitialized',
+    SET_DISPLAY_NAME: 'setDisplayName',
     PARTICIPANT_JOINED: 'participantJoined',
     PARTICIPANT_LEFT: 'participantLeft',
     MUTE_AUDIO: 'muteAudio',
@@ -44,6 +48,14 @@ function onMessage(event) {
 
     switch (receivedData.action) {
         // Switch the incoming messages
+        case ATHEER_MESSAGE.SET_DISPLAY_NAME:
+            var displayName = "Atheer User: LOCAL_USER";
+            if (receivedData.data) {
+                displayName = receivedData.data.username + displayNameSplit + receivedData.data.userhash;
+            }
+            APP.store.dispatch(updateSettings({displayName: displayName}));
+            break;
+
         case ATHEER_MESSAGE.MUTE_AUDIO:
             var status = true;
             if (receivedData.data && receivedData.data.status === false) {
