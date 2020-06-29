819f98a583cd0c3468f171409f91a293ab8cf1df
Prasad Kadiyala <pkadiyala@innominds.com>
2020-06-29 16:00:16 +0530

    converting initials to uppercase.

--- react/features/base/avatar/functions.js
+++ react/features/base/avatar/functions.js
@@ -57,6 +57,8 @@ export function getInitials(s: ?string) {
         names.forEach(name => {
             initials = initials ? initials.concat(name.charAt(0)) : name.charAt(0);
         });
+
+        initials = initials.toUpperCase();
     } else {
         initials = s;
     }
