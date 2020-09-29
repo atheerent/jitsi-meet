ab246e21fc120a5987f8a56902d4b697a2e44783
Sanjay Parmar <sparmar@atheerair.com>
2020-09-29 12:20:30 -0700

    enabling calls stats

--- storm_config.js
+++ storm_config.js
@@ -303,19 +303,19 @@ var config = {
     // This can be useful for debugging purposes (post-processing/analysis of
     // the webrtc stats) as it is done in the jitsi-meet-torture bandwidth
     // estimation tests.
-    // gatherStats: false,
+    gatherStats: true,
 
     // To enable sending statistics to callstats.io you must provide the
     // Application ID and Secret.
-    // callStatsID: '',
-    // callStatsSecret: '',
+    callStatsID: '426609706',
+    callStatsSecret: 'a8yliDCeA8S2:Lkn5nVwNp8E7TskAOtBE7q4jzIMjPQny6jkx2f8XU5E=',
 
     // enables callstatsUsername to be reported as statsId and used
     // by callstats as repoted remote id
-    // enableStatsID: false
+    enableStatsID: true,
 
     // enables sending participants display name to callstats
-    // enableDisplayNameInStats: false
+    enableDisplayNameInStats: true,
 
 
     // Privacy
